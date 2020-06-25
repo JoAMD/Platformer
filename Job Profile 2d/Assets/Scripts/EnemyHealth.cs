a27cc314a5e1ee677bc8e95d0a581c3beae18cfc
Davies <toludavid39@gmail.com>
2020-06-25 01:12:42 +0100

    Dash Changes

---
+++ Job Profile 2d/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private Animator anim;
+    private float health;
+    private float maxHealth = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+        anim.SetTrigger("Hit");
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Job Profile 2d/Assets/Scripts/HandleAttack.cs
+++ Job Profile 2d/Assets/Scripts/HandleAttack.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class HandleAttack : MonoBehaviour
 {
     private Animator anim;
+    private PlayerMovement player;
     private bool attack, slide = false;
     private Rigidbody2D rb;
     private float dashTime;
@@ -18,11 +20,14 @@ public class HandleAttack : MonoBehaviour
     private float AttackTime;
     private float attackRate = .5f;
     public LayerMask enemyLayers;
-    private float attackRange;
+    public float attackRange;
     public Transform attackPoint;
    
    
     private PlayerMovement movement;
+    private EnemyHealth _health;
+    private float damage = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,14 +50,14 @@ public class HandleAttack : MonoBehaviour
     }
     void HandleInput()
     {
-        if(Time.time >= AttackTime)
+        if (Time.time >= AttackTime)
         {
             if (Input.GetKeyDown(KeyCode.F))
             {
                 attack = true;
                 AttackTime = Time.time + attackRate;
             }
-       
+
         }
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
@@ -63,63 +68,73 @@ public class HandleAttack : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                direction = 1;
+                if (player._XMovement > 0)
+                {
+                    direction = 1;
+                }
+                else if (player._xMovemnt < 0)
+                {
+                    direction = 2;
+                }
             }
-            else if (Input.GetKeyDown(KeyCode.RightShift))
+            else
             {
-                direction = 2;
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = startDash;
+                    rb.velocity = Vector2.zero;
+                }
+                else {
+                    dashTime -= Time.deltaTime;
+                }
+
             }
         }
-        else
+        void Attack()
         {
-            if (dashTime <= 0)
+            if (attack)
             {
-                direction = 0;
-                dashTime = startDash;
-                rb.velocity = Vector2.zero; 
+                int index = Random.Range(1, 4);
+                anim.SetTrigger("attack" + index);
+                rb.velocity = new Vector2(0, 0);
+                attack = false;
+                Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+                foreach (Collider2D destructible in hitenemies)
+                {
+                    _health = destructible.GetComponent<EnemyHealth>();
+                    if (_health != null)
+                    {
+                        _health.takeDamage(damage);
+                    }
+
+                }
             }
-            else {
-                dashTime -= Time.deltaTime;
-            }
-              
         }
-    }
-    void Attack()
-    {
-        if (attack)
+        void Slide()
         {
-            int index = Random.Range(1, 4);
-            anim.SetTrigger("attack" + index);
-            rb.velocity = new Vector2(0,0);
-            attack = false;
-            Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-            foreach(Collider2D destructible in hitenemies)
+            if (slide)
             {
-                Debug.Log("enemy hit"); 
+                anim.SetBool("Slide", true);
+                slide = false;
+            } else if (!slide)
+            {
+                anim.SetBool("Slide", false);
             }
         }
-    }
-    void Slide()
-    {
-        if(slide)
+        void Dash()
         {
-            anim.SetBool("Slide", true);
-            slide = false;
-        }else if (!slide)
+            if (direction == 1)
+            {
+                rb.velocity = Vector2.left * dashSpeed * Time.deltaTime;
+            } else if (direction == 2)
+            {
+                rb.velocity = Vector2.right * dashSpeed * Time.deltaTime;
+            }
+        }
+        void OnDrawGizmos()
         {
-            anim.SetBool("Slide", false);
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
         }
     }
-    void Dash()
-    {
-        if(direction == 1)
-        {
-            rb.velocity = Vector2.left * dashSpeed * Time.deltaTime;
-        }else if(direction == 2)
-        {
-            rb.velocity = Vector2.right * dashSpeed * Time.deltaTime;
-        }
-    }
-   
-  
 }
